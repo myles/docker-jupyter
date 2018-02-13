6510828a8d9258c5e52c5923534231f9c1f655f7
Myles Braithwaite <me@mylesbraithwaite.com>
2018-02-13 10:04:50 -0500

    :construction: Did some work a while ago.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Docker Jupyter Notebook
 
 This is the Docker configuration for my Jupyter Notebook instance.
+
+## Docker Usage
+
+```bash
+$ docker-compose build
+$ docker-compose run
+```
--- repos.py
+++ repos.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
+import collections
 import json
 import os
 
 import click
-from git import Repo
+from git import Repo, GitCommandError
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
 
 with open('repos.json') as fobj:
     notebook_repos = json.load(fobj)
+
+if os.path.isfile('repos.private.json'):
+    with open('repos.private.json') as fobj:
+        notebook_repos.update(json.load(fobj))
+
+notebook_repos = collections.OrderedDict(sorted(notebook_repos.items()))
 
 
 @click.group()
@@ -36,25 +43,36 @@ def status():
 
         repo = Repo(full)
 
-        untracked_files_count = sum(1 for f in repo.untracked_files)
+        try:
+            untracked_files_count = sum(1 for f in repo.untracked_files)
 
-        commits_behind = repo.iter_commits('master..origin/master')
-        commits_behind_count = sum(1 for c in commits_behind)
+            commits_behind = repo.iter_commits('master..origin/master')
+            commits_behind_count = sum(1 for c in commits_behind)
 
-        commits_ahead = repo.iter_commits('origin/master..master')
-        commits_ahead_count = sum(1 for c in commits_ahead)
+            commits_ahead = repo.iter_commits('origin/master..master')
+            commits_ahead_count = sum(1 for c in commits_ahead)
+
+            repo_error = False
+        except GitCommandError:
+            repo_error = True
 
         if commits_behind_count:
-            click.echo('{0} is {1} commits behind.'.format(
-                local, commits_behind_count))
+            click.secho('{0} is {1} commits behind.'.format(
+                local, commits_behind_count), fg='blue')
 
-        if commits_ahead_count:
-            click.echo('{0} is {1} commits ahead.'.format(
-                local, commits_ahead_count))
+        elif commits_ahead_count:
+            click.secho('{0} is {1} commits ahead.'.format(
+                local, commits_ahead_count), fg='blue')
 
-        if untracked_files_count:
-            click.echo('{0} has {1} untracked files.'.format(
-                local, untracked_files_count))
+        elif untracked_files_count:
+            click.secho('{0} has {1} untracked files.'.format(
+                local, untracked_files_count), fg='blue')
+
+        elif repo_error:
+            click.secho('{0} repo has an error.'.format(local), fg='red')
+
+        else:
+            click.secho('{0} everything is awesome.'.format(local), fg='green')
 
 
 if __name__ == '__main__':
